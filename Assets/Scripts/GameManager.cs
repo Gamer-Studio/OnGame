7078ac9f3bfa0e5f4b7214f093264f483223d0c1
AmteurPotion <1k0j2i4@gmail.com>
2025-05-07 19:56:23 +0900

    [Feat] 인트로 씬 업데이트
    * 게임 매니저 DDOL 싱글톤화
    * 기초 오브젝트 설정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,5 +6,16 @@ namespace OnGame
 { 
   public class GameManager : MonoBehaviour
   {
+    public static GameManager Instance { get; private set; }
+    
+    private void Awake()
+    {
+      if (Instance == null)
+      {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+      }
+      else Destroy(gameObject);
+    }
   }
 }
