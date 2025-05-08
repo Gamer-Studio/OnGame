3bf553aa077916172c3f7d9c8f71b34b75d98edf
AmteurPotion <1k0j2i4@gmail.com>
2025-05-09 03:56:04 +0900

    [Feat] 기초 플레이어 조작 구현

--- Assets/Scenes/World/Player.cs
+++ Assets/Scenes/World/Player.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using OnGame.Prefabs.Entities;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,13 +8,29 @@ namespace OnGame.Scenes.World
   public class Player : Entity
   {
     public Vector2 direction;
-    public float speed;
-    private void Start()
+    public float speed = 10;
+    public float cameraSpeed = 10;
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private new CinemachineVirtualCamera camera;
+
+    private void Update()
     {
+      var scroll = Input.GetAxis("Mouse ScrollWheel");
+
+      if (scroll != 0)
+        camera.m_Lens.OrthographicSize = Mathf.Clamp(
+          camera.m_Lens.OrthographicSize + scroll > 0 ? +cameraSpeed : -cameraSpeed
+          , 10, 100);
     }
-    
+
+    private void FixedUpdate()
+    {
+      body.AddForce(direction * speed, ForceMode2D.Impulse);
+    }
+
     private void OnMove(InputValue value)
     {
+      direction = value.Get<Vector2>();
     }
   }
 }
